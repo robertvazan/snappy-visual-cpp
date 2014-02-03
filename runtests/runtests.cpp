0a9a24ec52856efb05fd3969d482681b2b97a081
Robert Vazan <robvcode@gmail.com>
2014-02-03 22:47:26 +0100

    Test runner

---
+++ runtests/runtests.cpp
@@ -0,0 +1,12 @@
+// runtests.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+extern "C" int snappy_unittests(int argc, char** argv);
+
+int main(int argc, char* argv[])
+{
+	return snappy_unittests(argc, argv);
+}
+
